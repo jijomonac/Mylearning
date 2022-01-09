47497dddd7f061a196670998be097d87fccf314b
Jijo <jijomonac@gmail.com>
2022-01-09 14:01:30 +0530

    Test b2 (#14)
    * Test b1 (#11)
    
    * commit test_b1 5_1
    
    * commit test_b1 5_2
    
    * commit testb_1 6_1
    
    * commit master 7_1
    
    * //commit testb 7_1
    
    * //commit testb 7_2
    
    * //commit testb2 1
    
    * //commit testb2 2

--- src/com/jac/junit/mokito/Database.java
+++ src/com/jac/junit/mokito/Database.java
@@ -7,7 +7,7 @@ package com.jac.junit.mokito;
 public class Database {
     public boolean isAvailable() {
         // currently not implemented, as this is just demo used in a software test
-        //commit test_b1 1
+
         return false;
     }
     public int getUniqueId() {
